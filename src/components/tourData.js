c89c2b52d950abe9a6f6fddea9fa19993ca85ddc
German Novotny <german.novotny@gmail.com>
2024-10-25 15:59:12 -0300

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,58 @@
+export const tourData = [
+    {
+        id: 1, 
+        city: "brazil", 
+        img: "./img/brazil.jpg", 
+        name: "brazil morro tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    }, 
+    {
+        id: 2, 
+        city: "mexico", 
+        img: "./img/mexico.jpg", 
+        name: "mexico maya tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    }, 
+    {
+        id: 3, 
+        city: "rome", 
+        img: "./img/italyrome.jpg", 
+        name: "italy tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    },     
+    {
+        id: 4, 
+        city: "paris", 
+        img: "./img/paris.jpg", 
+        name: "paris lights tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    }, 
+    {
+        id: 5, 
+        city: "london", 
+        img: "./img/london.jpg", 
+        name: "london royal palace tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    }, 
+    {
+        id: 6, 
+        city: "japan", 
+        img: "./img/japan.jpg", 
+        name: "japan sushi tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    }, 
+    {
+        id: 7, 
+        city: "new york", 
+        img: "./img/newyork.jpg", 
+        name: "new york bridge tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    }, 
+    {
+        id: 8, 
+        city: "jamaica", 
+        img: "./img/jamaica.jpg", 
+        name: "jamaica tour", 
+        info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, repellendus!"
+    }
+];
